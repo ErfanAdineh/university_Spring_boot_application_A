7376ae1ea3d467ee38193de5df7e8e01aa24d2ba
erfan adine <erfan.developer2001@gmail.com>
2023-01-17 22:39:05 +0330

    converters for service part 2

--- src/main/java/com/university/universityapplication_erfanadine/service/TeacherService.java
+++ src/main/java/com/university/universityapplication_erfanadine/service/TeacherService.java
@@ -1,7 +1,113 @@
 package com.university.universityapplication_erfanadine.service;
 
+import com.university.universityapplication_erfanadine.dto.TeacherDto;
+import com.university.universityapplication_erfanadine.entity.FoodWeek;
+import com.university.universityapplication_erfanadine.entity.Lesson;
+import com.university.universityapplication_erfanadine.entity.Teacher;
+import com.university.universityapplication_erfanadine.entity.UnitOfUniversity;
+import com.university.universityapplication_erfanadine.repository.FoodWeekRepo;
+import com.university.universityapplication_erfanadine.repository.LessonRepo;
+import com.university.universityapplication_erfanadine.repository.UnitOfUniversityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TeacherService {
+
+
+    @Autowired
+    private LessonRepo lessonRepo;
+
+    @Autowired
+    private FoodWeekRepo foodWeekRepo;
+
+    @Autowired
+    private UnitOfUniversityRepo unitOfUniversityRepo;
+
+    public TeacherDto convertTeacherModelToTeacherDto(Teacher teacher) {
+
+        List<Long> foodWeeks_Longs = new ArrayList<>();
+        List<Long> lessons_Longs = new ArrayList<>();
+        List<Long> unitOfUniversities_Longs = new ArrayList<>();
+
+
+        if ( teacher == null ) {
+            return null;
+        }
+
+        TeacherDto teacherDto = new TeacherDto();
+
+        teacherDto.setId( teacher.getId() );
+
+        List<FoodWeek> foodWeeks = teacher.getFoodWeeks();
+        if ( foodWeeks != null ) {
+            for (FoodWeek foodWeek : foodWeeks) {
+                foodWeeks_Longs.add(foodWeek.getId());
+            }
+            teacherDto.setFoodWeeks_Id(foodWeeks_Longs);
+        }
+        List<Lesson> lessons = teacher.getLessons();
+        if ( lessons != null ) {
+            for (Lesson lesson :lessons ) {
+                lessons_Longs.add(lesson.getId());
+            }
+            teacherDto.setLessons_Id(lessons_Longs);
+        }
+        List<UnitOfUniversity> unitOfUniversities = teacher.getUnitOfUniversities();
+        if ( unitOfUniversities != null ) {
+            for (UnitOfUniversity unitOfUniversity : unitOfUniversities) {
+                unitOfUniversities_Longs.add(unitOfUniversity.getId());
+            }
+            teacherDto.setUnitOfUniversities_Id(unitOfUniversities_Longs);
+        }
+
+        return teacherDto;
+    }
+
+
+    public Teacher convertTeacherDtoToTeacherModel(TeacherDto teacherDto) {
+        List<FoodWeek> foodWeeks = new ArrayList<>();
+        List<Lesson> lessons = new ArrayList<>();
+        List<UnitOfUniversity> unitOfUniversities = new ArrayList<>();
+
+        if ( teacherDto == null ) {
+            return null;
+        }
+
+        Teacher teacher = new Teacher();
+
+        teacher.setId(teacherDto.getId());
+
+        List<Long> foodWeek_longs = teacherDto.getFoodWeeks_Id();
+        if ( foodWeek_longs != null ) {
+            for (Long foodWeekId : foodWeek_longs) {
+                foodWeeks.add(foodWeekRepo.findById(foodWeekId).orElse(null));
+            }
+            teacher.setFoodWeeks(foodWeeks);
+        }
+
+
+        List<Long> lesson_longs = teacherDto.getLessons_Id();
+        if ( lesson_longs != null ) {
+
+            for (Long lesson : lesson_longs) {
+                lessons.add(lessonRepo.findById(lesson).orElse(null));
+            }
+            teacher.setLessons(lessons);
+        }
+
+
+        List<Long> unitOfUniversities_longs = teacherDto.getUnitOfUniversities_Id();
+        if ( unitOfUniversities_longs != null ) {
+            for (Long unitOfUniversity : unitOfUniversities_longs) {
+                unitOfUniversities.add(unitOfUniversityRepo.findById(unitOfUniversity).orElse(null));
+            }
+            teacher.setUnitOfUniversities( unitOfUniversities );
+        }
+
+        return teacher;
+    }
 }
--- src/main/java/com/university/universityapplication_erfanadine/service/UnitOfUniversityService.java
+++ src/main/java/com/university/universityapplication_erfanadine/service/UnitOfUniversityService.java
@@ -1,7 +1,96 @@
 package com.university.universityapplication_erfanadine.service;
 
+import com.university.universityapplication_erfanadine.dto.UnitOfUniversityDto;
+import com.university.universityapplication_erfanadine.entity.Lesson;
+import com.university.universityapplication_erfanadine.entity.Teacher;
+import com.university.universityapplication_erfanadine.entity.UnitOfUniversity;
+import com.university.universityapplication_erfanadine.repository.LessonRepo;
+import com.university.universityapplication_erfanadine.repository.TeacherRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UnitOfUniversityService {
+
+    @Autowired
+    private LessonRepo lessonRepo;
+
+    @Autowired
+    private TeacherRepo teacherRepo;
+
+    public UnitOfUniversityDto convertUnitOfUniversityModelToUnitOfUniversityDto(UnitOfUniversity unitOfUniversity) {
+
+        List<Long> teacher_longs = new ArrayList<>();
+        List<Long> lesson_longs = new ArrayList<>();
+
+
+        if (unitOfUniversity == null) {
+            return null;
+        }
+
+        UnitOfUniversityDto unitOfUniversityDto = new UnitOfUniversityDto();
+
+        unitOfUniversityDto.setId(unitOfUniversity.getId());
+        unitOfUniversityDto.setNameOfUniversity_Unit(unitOfUniversity.getNameOfUniversity_Unit());
+        unitOfUniversityDto.setCodeOfUnit(unitOfUniversity.getCodeOfUnit());
+
+        List<Teacher> teachers = unitOfUniversity.getTeachers();
+        if (teachers != null) {
+
+            for (Teacher teacher : teachers) {
+                teacher_longs.add(teacher.getId());
+            }
+            unitOfUniversityDto.setTeachers_Id(teacher_longs);
+        }
+
+        List<Lesson> lessons = unitOfUniversity.getLessons();
+        if (lessons != null) {
+            for (Lesson lesson : lessons) {
+                lesson_longs.add(lesson.getId());
+            }
+            unitOfUniversityDto.setLessons_Id(lesson_longs);
+        }
+
+        return unitOfUniversityDto;
+    }
+
+
+    public UnitOfUniversity convertUnitOfUniversityDtoToUnitOfUniversityModel(UnitOfUniversityDto unitOfUniversityDto) {
+
+        List<Teacher> teachers = new ArrayList<>();
+        List<Lesson> lessons = new ArrayList<>();
+
+        if (unitOfUniversityDto == null) {
+            return null;
+        }
+
+        UnitOfUniversity unitOfUniversity = new UnitOfUniversity();
+
+        unitOfUniversity.setId(unitOfUniversityDto.getId());
+        unitOfUniversity.setNameOfUniversity_Unit(unitOfUniversityDto.getNameOfUniversity_Unit());
+        unitOfUniversity.setCodeOfUnit(unitOfUniversityDto.getCodeOfUnit());
+
+        List<Long> teachers_longs = unitOfUniversityDto.getTeachers_Id();
+        if (teachers_longs != null) {
+
+            for (Long teacherId : teachers_longs) {
+                teachers.add(teacherRepo.findById(teacherId).orElse(null));
+            }
+            unitOfUniversity.setTeachers(teachers);
+        }
+
+        List<Long> lessons_longs = unitOfUniversityDto.getLessons_Id();
+        if (lessons_longs != null) {
+
+            for (Long lessonId : lessons_longs) {
+                lessons.add(lessonRepo.findById(lessonId).orElse(null));
+            }
+            unitOfUniversity.setLessons(lessons);
+        }
+
+        return unitOfUniversity;
+    }
 }
